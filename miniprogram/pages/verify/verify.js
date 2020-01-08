0f671ce4c02c198f187f7755df3e373976894b54
欧世乐 <ouyangshile@qq.com>
2020-01-08 21:20:16 +0800

    1

--- miniprogram/pages/verify/verify.js
+++ miniprogram/pages/verify/verify.js
@@ -2,6 +2,7 @@ var app=getApp();
 var userInfo;//存放用户信息
 var count;//统计共有多少学生信息需要审核
 var finish=false;//当前用户所有证书是否审核完毕
+var check=0;
 Page({
   
    
@@ -118,5 +119,57 @@ Page({
       }
     })
   },
+  disagree:function(res){
+    var that=this;
+    check=0;
+    wx.showModal({
+      title: '提示',
+      content: '确定拒绝吗',
+      success:function(res){
+        if(res.confirm){
+          userInfo['category'].splice(0,1);
+          console.log(userInfo)
+          if (userInfo['category'].length==0){
+            check=1;
+            count-=1;
+          }
+          console.log(check)
+          wx.cloud.callFunction({
+            name:'agreeUser',
+            data:{
+              _id: userInfo['_id'],
+              category: userInfo['category'],
+              check:check
+            },
+            success:function(res){
+              console.log("已拒绝该证书申请")
+              if(count!=0){
+                wx.cloud.callFunction({
+                  name: 'selectAllInfo',
+                  data: {},
+                  success: function (res) {
+                    userInfo = res.result.data[0];
+                    for (var j = 0; j < userInfo['category'].length; j++) {
+                      if (userInfo['category'][j][0]['check'] == 1) {
+                        userInfo['category'].splice(j, 1);
+                      }
+                    }
+                    console.log(userInfo)
+                    that.setData({
+                      userInfo
+                    })
+                  }
+                })
+              }
+              that.setData({
+                count,
+                userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  }
   
 })
